977f62a851fbda229f92693195d4e5747264c03e
FelipeAz <felipe9_azevedo@hotmail.com>
2021-11-25 23:55:32 -0500

    Changed orders route with unnecessary ID

--- build/server/orders/router/build/item_routes.go
+++ build/server/orders/router/build/item_routes.go
@@ -7,7 +7,7 @@ import (
 
 func ItemRoutes(rg *gin.RouterGroup, itemHandler handler.ItemHandler) {
 	r := rg.Group("/orders")
-	r.GET("/:id/book", itemHandler.Get)
+	r.GET("/", itemHandler.Get)
 	r.GET("/:id", itemHandler.Find)
 	r.POST("/", itemHandler.Create)
 	r.PUT("/:id", itemHandler.Update)
