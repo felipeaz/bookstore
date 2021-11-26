6ba2d3fc89da831968c28baf96985744cd639e63
FelipeAz <felipe9_azevedo@hotmail.com>
2021-11-26 00:33:20 -0500

    Renamed FirstName & LastName properties to AuthorFirstName & AuthorLastName

--- internal/app/domain/inventory/books/model/book_model.go
+++ internal/app/domain/inventory/books/model/book_model.go
@@ -2,22 +2,20 @@ package model
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
 // Book contains all Book's table properties.
 type Book struct {
-	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Title     string    `json:"title" binding:"required"`
-	FirstName string    `json:"firstName" binding:"required"`
-	LastName  string    `json:"lastName" binding:"required"`
-	Amount    int64     `json:"amount"`
-	CreatedAt time.Time `time_format:"2006-01-02 15:04:05"`
-	UpdatedAt time.Time `time_format:"2006-01-02 15:04:05"`
+	ID              uint      `json:"id" gorm:"primaryKey;autoIncrement;not null"`
+	Title           string    `json:"title" binding:"required"`
+	AuthorFirstName string    `json:"authorFirstName" binding:"required"`
+	AuthorLastName  string    `json:"authorLastName" binding:"required"`
+	Amount          int64     `json:"amount"`
+	CreatedAt       time.Time `time_format:"2006-01-02 15:04:05"`
+	UpdatedAt       time.Time `time_format:"2006-01-02 15:04:05"`
 }
 
 func (b Book) GetAuthorName() string {
-	s := fmt.Sprintf("%s %s", b.FirstName, b.LastName)
-	return strings.TrimSpace(s)
+	return fmt.Sprintf("%s %s", b.AuthorFirstName, b.AuthorLastName)
 }
